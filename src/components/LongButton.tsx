59632ccd4207740e8e7dc640e8fcb22ffc89e638
leo <leoyh.97@gmail.com>
2023-06-08 20:37:00 +0800

    added import to necessary files

--- src/components/LongButton.tsx
+++ src/components/LongButton.tsx
@@ -1,6 +1,7 @@
 import Button from '@mui/material/Button';
 import { theme } from '../styles/Theme';
 import { ThemeProvider } from '@mui/material/styles';
+import React from 'react';
 
 const LongButton = (prop: any) => {
   return (
