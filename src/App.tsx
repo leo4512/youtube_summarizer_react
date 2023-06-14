9cf745ac510b224a148e7c1d2c43c208916afa81
leo <leoyh.97@gmail.com>
2023-06-14 21:32:12 +0800

    added hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 // import Button from "@mui/material/Button";
 import React from "react";
-import {WelcomePage} from "./pages/index"
+import AppRouter from "./router";
 
 function App() {
 	return (
-		<WelcomePage/>
+		<AppRouter />
 		// <div className="App">
 		// 	<div>
 		// 		<Button variant="contained">Hello World</Button>
