3c4655cfb6860a99eea48876927f5fc888397ddd
leo <leoyh.97@gmail.com>
2023-06-22 20:57:10 +0800

    added setting page

---
+++ src/pages/settingPage.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { TextInput, LongButton } from "../components/index";
+import IconButton from '@mui/material/IconButton';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+const StyledForm = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
+
+// const StyledDiv = styled.div`
+// 	margin-top: 20px;
+// `;
+
+const SettingDiv = styled.div`
+	width: 302px;
+	height: 255px;
+	margin: auto;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+`;
+
+const BackButton = styled(IconButton)`
+	position: absolute;
+	top: 10px;
+	left: 10px;
+`;
+
+const SettingPage = () => {
+	const [apiKey, setApiKey] = useState("");
+	const [language, setLanguage] = useState("");
+
+    const handleLanguageChange = (value: string) => {
+		setLanguage(value);
+	};
+
+	const handleApiChange = (value: string) => {
+		setApiKey(value);
+	};
+	console.log(apiKey);
+
+    const handleSubmit = (): void => {
+        chrome.storage.sync.set({ apiKey, language }, () => {
+            console.log('The apiKey and language are saved.');
+        });
+    };
+
+    const goBack = (): void => {
+		// Logic to go back, for example:
+		window.history.back();
+	};
+
+	return (
+		<SettingDiv>
+            <BackButton onClick={goBack}>
+				<ArrowBackIcon />
+			</BackButton>
+			<StyledForm>
+				<TextInput label="Language" onChange={handleLanguageChange} />
+				<TextInput label="API key" onChange={handleApiChange} />
+			</StyledForm>
+            <LongButton onClick={handleSubmit}/>
+		</SettingDiv>
+	);
+};
+
+export default SettingPage;
