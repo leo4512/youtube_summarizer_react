986ce2496edc74549cb0fa9c9b3898c28b20a237
Athena Wu <wushuchen113@gmail.com>
2023-06-23 22:44:59 +0930

    styled: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
 import { WelcomePage, SettingPage } from "./pages";
+import GlobalStyle from "./styles/GlobalStyle";
 
 function App() {
   return (
-    <HashRouter basename="/">
-      <Routes>
-        <Route path="/" element={<WelcomePage />} />
-        <Route path="/setting" element={<SettingPage />} />
-      </Routes>
-    </HashRouter>
+    <div>
+      {" "}
+      <GlobalStyle />
+      <HashRouter basename="/">
+        <Routes>
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/setting" element={<SettingPage />} />
+        </Routes>
+      </HashRouter>
+    </div>
   );
 }
 
